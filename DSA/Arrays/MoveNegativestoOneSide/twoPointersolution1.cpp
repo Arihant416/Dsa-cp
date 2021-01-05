7478bf3a1d73f2b82336dd94099270c3d1504c1d
Arihant416 <arihantjain416@gmail.com>
2021-01-05 20:34:24 +0530

    Two pointer 1

---
+++ DSA/Arrays/MoveNegativestoOneSide/twoPointersolution1.cpp
@@ -0,0 +1,72 @@
+/*
+Author : Arihant Jain
+github:https://github.com/Arihant416
+linkedin : https://www.linkedin.com/in/arihant416
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define ull unsigned ll
+#define PB push_back
+#define MP make_pair
+#define flash ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr)
+#define vi vector<int>
+#define vl vector<ll>
+#define vll vector<ull>
+#define mpi map<int, int>
+#define mpl map<ll, ll>
+#define mpll map<ull, ull>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define all(a) begin(a), end(a)
+#define maxEl(a) *max_element(all(a))
+#define minEl(a) *min_element(all(a))
+#define uimap unordered_map<int, int>
+#define ulmap unordered_map<ll, ll>
+#define mppii map<pi, int>
+
+void print(vi &A, int N)
+{
+    for (int &i : A)
+    {
+        cout << i << " ";
+    }
+}
+
+void solve(vi &A, int N)
+{
+    int low(0), high(N - 1);
+    while (low <= high)
+    {
+        if (A[low] < 0 && A[high] < 0)
+            low++;
+        else if (A[low] > 0 && A[high] < 0)
+        {
+            swap(A[low++], A[high--]);
+        }
+        else if (A[low] > 0 && A[high] > 0)
+        {
+            high--;
+        }
+        else
+        {
+            low++;
+            high--;
+        }
+    }
+}
+
+int32_t main()
+{
+    flash;
+    int N;
+    cin >> N;
+    vi A(N, 0);
+    for (int &i : A)
+    {
+        cin >> i;
+    }
+    solve(A, N);
+    print(A, N);
+    return 0;
+}
