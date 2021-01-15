e02ab6866d04fbd7d5910daa6126b293a66201d2
Arihant416 <arihantjain416@gmail.com>
2021-01-15 11:45:51 +0530

    Add Iterative Solution

---
+++ DSA/Strings/countAndSay/iterative.cpp
@@ -0,0 +1,66 @@
+/*
+Author : Arihant Jain
+github:https://github.com/Arihant416
+linkedin : https://www.linkedin.com/in/arihant416
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define ull unsigned ll
+#define PB push_back
+#define MP make_pair
+#define flash ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr)
+#define vi vector<int>
+#define vl vector<ll>
+#define vll vector<ull>
+#define mpi map<int, int>
+#define mpl map<ll, ll>
+#define mpll map<ull, ull>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define all(a) begin(a), end(a)
+#define maxEl(a) *max_element(all(a))
+#define minEl(a) *min_element(all(a))
+#define uimap unordered_map<int, int>
+#define ulmap unordered_map<ll, ll>
+#define mppii map<pi, int>
+
+class Solution
+{
+public:
+    string countAndSay(int N);
+};
+
+string Solution::countAndSay(int N)
+{
+    if (N == 0)
+        return "";
+    string res = "1";
+    while (--N)
+    {
+        string current = "";
+        for (int i = 0; i < res.size(); i++)
+        {
+            int count = 1;
+            while ((i + 1 < res.size()) && (res[i] == res[i + 1]))
+            {
+                count++;
+                i++;
+            }
+            current += to_string(count) + res[i];
+        }
+        res = current;
+    }
+    return res;
+}
+
+int32_t main()
+{
+    flash;
+
+    int N;
+    cin >> N;
+    Solution ob;
+    cout << ob.countAndSay(N);
+    return 0;
+}
