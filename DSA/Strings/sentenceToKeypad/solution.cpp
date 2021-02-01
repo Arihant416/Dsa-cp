b217c8a2f9d8f2d1961d50aba31585998917e4ad
Arihant416 <arihantjain416@gmail.com>
2021-02-01 13:06:20 +0530

    Add KeypadToString

---
+++ DSA/Strings/sentenceToKeypad/solution.cpp
@@ -0,0 +1,50 @@
+/*
+Author : Arihant Jain
+github:https://github.com/Arihant416
+linkedin : https://www.linkedin.com/in/arihant416
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define ull unsigned ll
+#define PB push_back
+#define MP make_pair
+#define flash ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr)
+#define vi vector<int>
+#define vl vector<ll>
+#define vll vector<ull>
+#define mpi map<int, int>
+#define mpl map<ll, ll>
+#define mpll map<ull, ull>
+#define pi pair<int, int>
+#define pl pair<ll, ll>
+#define all(a) begin(a), end(a)
+#define maxEl(a) *max_element(all(a))
+#define minEl(a) *min_element(all(a))
+#define uimap unordered_map<int, int>
+#define ulmap unordered_map<ll, ll>
+#define mppii map<pi, int>
+
+vector<string> lookup = {"2", "22", "222", "3", "33", "333", "4", "44", "444", "5", "55", "555", "6", "66", "666", "7", "77", "777", "7777", "8", "88", "888", "9", "99", "999", "9999"};
+
+string keyStrokes(string &line)
+{
+    string result;
+    for (char c : line)
+    {
+        if (c == ' ')
+            result.PB('0');
+        else
+            result += lookup[c - 'A'];
+    }
+    return result;
+}
+
+int32_t main()
+{
+    flash;
+    string line;
+    getline(cin, line);
+    cout << keyStrokes(line) << endl;
+    return 0;
+}
