59091ab4542d47d8edc066f338261ffe484ba4c4
Arihant416 <arihantjain416@gmail.com>
2021-02-19 14:08:12 +0530

    Life Universe and Everything

---
+++ DSA/dsaSeriesCodechef/week1/test.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	while(true){
+	    int num;
+	    cin >> num;
+	    if(num == 42){
+	        break;
+	    }
+	    cout << num << endl;
+	}
+	return 0;
+}
